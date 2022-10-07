696bf65b0ef9ab7a2c911f868803485a258226a8
Ryan Omasta <ryand@emeraldsys.xyz>
2022-10-06 21:20:21 -0600

    Add boilerplate

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,14 +3,38 @@ import * as React from "react";
 import Head from "next/head";
 
 const MyApp = ({ Component, pageProps, router }) => {
+    React.useEffect(() => {
+        if (typeof window === undefined) {
+            return;
+        }
+
+        window.addEventListener("resize", (e) => {
+            let scale, windowAspect;
+            
+            let innerWidth = window.innerWidth;
+            let innerHeight = window.innerHeight;
+
+            windowAspect = innerWidth / innerHeight;
+
+            if (windowAspect >= 4/3) {
+                scale = innerHeight / window.outerHeight();
+            } else {
+                scale = innerWidth / window.outerWidth();
+            }
+
+            document.querySelector("#main").classList.add(`scale-[${scale}]`);
+        });
+    }, []);
+
     return (
         <>
             <Head>
                 <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
                 <title>Weatherscan</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </Head>
 
-            <div>
+            <div id="main" className="relative top-1/2 left-1/2 overflow-hidden transform -translate-x-1/2 -translate-y-1/2 w-[1440px] h-[1080px] will-change-transform">
                 <Component {...pageProps} key={router.pathname} />
             </div>
         </>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,16 @@
 import * as React from "react";
 
 const Index = () => {
-    return <h1 className="text-white">Hello World!</h1>;
+    return (
+        <div className="h-[23.5%] w-full absolute bottom-0 bg-[#161418]">
+            <div className="absolute w-[400px] left-[8.05%] top-[4.6%] flex flex-row flex-col">
+                <div className="whitespace-nowrap relative font-['Frutiger'] font-semibold text-[#d8c422] text-[31.5px] tracking-[1px] pt-6">headend id:</div>
+                <div className="whitespace-nowrap relative font-['Frutiger'] font-semibold text-[#d8c422] text-[31.5px] tracking-[1px] pt-6">serial number:</div>
+                <div className="whitespace-nowrap relative font-['Frutiger'] font-semibold text-[#d8c422] text-[31.5px] tracking-[1px] pt-6">location name:</div>
+                <div className="whitespace-nowrap relative font-['Frutiger'] font-semibold text-[#d8c422] text-[31.5px] tracking-[1px] pt-6">affiliate name:</div>
+            </div>
+        </div>
+    );
 };
 
 export default Index;

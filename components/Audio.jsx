3abd80ec3defa30f079fbb133d419908b019de72
Ryan Omasta <ryand@emeraldsys.xyz>
2022-10-10 01:52:36 -0600

    Add audio button

--- components/Audio.jsx
+++ components/Audio.jsx
@@ -1,23 +1,25 @@
 import * as React from "react";
 import { useAudioPlayer } from "react-use-audio-player";
+import { MdPlayArrow, MdPause } from "react-icons/md";
 
 const randNum = (min, max) => {
-    return Math.floor(Math.random() * (max - min) + min);
+    return Math.round(Math.random() * (max - min) + min);
 };
 
 const Audio = () => {
-    const [fileIdx, setFileIdx] = React.useState(randNum(1, 4));
+    const [fileIdx, setFileIdx] = React.useState(randNum(1, 5));
     const [file, setFile] = React.useState(`/music/${encodeURIComponent(`Weatherscan Track ${fileIdx}.mp3`)}`);
 
-    const { ready, loading, playing, load } = useAudioPlayer({
+    const { togglePlayPause, ready, loading, playing, load } = useAudioPlayer({
         src: file,
         format: "mp3",
         autoplay: true,
         onplay: () => console.log(`Playing ${file}`),
         onend: () => {
-            let rand = randNum(1, 4);
+            console.log(`${file} has ended`);
+            let rand = randNum(1, 5);
             while (rand === fileIdx) {
-                rand = randNum(1, 4);
+                rand = randNum(1, 5);
             }
             setFileIdx(rand);
         }
@@ -28,7 +30,13 @@ const Audio = () => {
         setFile(`/music/${encodeURIComponent(`Weatherscan Track ${fileIdx}.mp3`)}`);
     }, [fileIdx]);
 
-    return null;
+    return (
+        <div id="audio" className="fixed right-0 bottom-0 flex flex-row flex-nowrap">
+            <button onClick={togglePlayPause}>
+                {playing ? <MdPause className="text-white p-2 w-[36px] h-[36px]" /> : <MdPlayArrow className="text-white p-2 w-[36px] h-[36px]" />}
+            </button>
+        </div>
+    );
 };
 
 export default Audio;

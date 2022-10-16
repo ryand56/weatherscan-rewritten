4a83178f37e22299b2af44c191ba4553c9e79ade
Ryan Omasta <ryand@emeraldsys.xyz>
2022-10-16 15:36:47 -0600

    [skip deploy] Add slide boilerplates

--- components/Slides/Containers/Slides/CityInfo/index.jsx
+++ components/Slides/Containers/Slides/CityInfo/index.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import FrostPane from "./FrostPane";
+import Forecast from "./Forecast";
 
 const SlideCityInfo = () => (
     <>
@@ -26,22 +26,7 @@ const SlideCityInfo = () => (
                 id="tempunavailable"
                 className="transform translate-x-0 translate-y-60px scale-x-114-5 scale-y-100 origin-left absolute whitespace-nowrap font-frutiger57-cond pt-noreport-t pl-tempunavailable-l text-white text-tempunavailable text-shadow z-noreport hidden"
             >Temporarily Unavailable</div>
-            <div id="infoslide-content-forecast" className="h-full min-h-full w-full flex-1 z-0 flex flex-row top-forecast text-forecast hidden">
-                <FrostPane extraParent={[
-                    "p-info-frost-pane",
-                    "h-info-frost-pane"
-                ]}>
-                    <div
-                        id="title"
-                        className="font-frutiger57-cond text-frost-pane-title text-severe-frost-pane-title transform scale-x-117 scale-y-100 origin-left tracking-severe-frost-pane-title capitalize"
-                    />
-                    <div
-                        id="content"
-                        className="font-frutiger57-cond h-frost-pane-content max-h-frost-pane-content transform -translate-x-2.5 -translate-y-2.5 scale-x-115 scale-y-100 origin-left tracking-frost-pane-content"
-                        style={{ fontSize: "52px" }}
-                    />
-                </FrostPane>
-            </div>
+            <Forecast />
         </div>
     </>
 );
---
+++ components/Slides/Containers/Slides/CityIntro/index.jsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+const CityIntro = () => (
+    <div>
+        
+    </div>
+);
+
+export default CityIntro;

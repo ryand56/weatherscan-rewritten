88f08f68552febd18e6362c8a7cd3110ecaacbb1
Ryan Omasta <ryand@emeraldsys.xyz>
2022-11-15 14:15:44 -0700

    Add missing file

---
+++ components/Slides/Containers/Slides/City/CityInfo/Near.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import FrostPane from "../../FrostPane";
+
+const Forecast = () => (
+    <div id="infoslide-content-forecast" className="h-full min-h-full w-full flex-1 z-0 flex flex-row top-forecast text-forecast hidden">
+        <FrostPane extraParent={[
+            "p-info-frost-pane",
+            "h-info-frost-pane"
+        ]}>
+            <div
+                id="title"
+                className="font-frutiger57-cond text-frost-pane-title text-severe-frost-pane-title transform scale-x-117 scale-y-100 origin-left tracking-severe-frost-pane-title capitalize"
+            />
+            <div
+                id="content"
+                className="font-frutiger57-cond h-frost-pane-content max-h-frost-pane-content transform -translate-x-2.5 -translate-y-2.5 scale-x-115 scale-y-100 origin-left tracking-frost-pane-content"
+                style={{ fontSize: "52px" }}
+            />
+        </FrostPane>
+    </div>
+);
+
+export default Forecast;

9d4406b03187c21ee2114591bf3d4b823e902dbf
Ryan Omasta <ryand@emeraldsys.xyz>
2022-12-11 23:14:32 -0700

    Add extended forecast boilerplate

---
+++ components/Slides/Containers/Slides/City/CityInfo/Extended.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+// Extended forecasts
+const Extended = () => (
+    <div>Extended</div>
+);
+
+export default Extended;
--- components/Slides/Containers/Slides/City/CityInfo/index.tsx
+++ components/Slides/Containers/Slides/City/CityInfo/index.tsx
@@ -7,6 +7,7 @@ import NoReport from "./NoReport";
 import Unavailable from "./Unavailable";
 import Detailed from "./Detailed";
 import Near from "./Near";
+import Extended from "./Extended";
 
 const SlideCityInfo = ({ next, location, currentCityInfo, setVocal }: SlideProps) => {
     const [slideState, slideDispatch] = React.useReducer(SlideshowReducer, { index: 0 });
@@ -21,6 +22,8 @@ const SlideCityInfo = ({ next, location, currentCityInfo, setVocal }: SlideProps
                 />;
             case 1:
                 return <Near />;
+            case 2:
+                return <Extended />;
             default:
                 return null;
         }
@@ -28,11 +31,11 @@ const SlideCityInfo = ({ next, location, currentCityInfo, setVocal }: SlideProps
 
     React.useEffect(() => {
         let timeout = setTimeout(() => {
-            /* if (slideState.index >= 1) setTimeout(() => {
+            /* if (slideState.index >= 2) setTimeout(() => {
                 next();
                 slideDispatch({ type: ActionType.SET, payload: 0 });
             }, 800); */
-            if (slideState.index >= 1) {
+            if (slideState.index >= 2) {
                 next();
                 slideDispatch({ type: ActionType.SET, payload: 0 });
                 return;

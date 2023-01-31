90ce1f312a7fbed8e7c05362111cc877c70e0acf
Ryan Omasta <ryand@emeraldsys.xyz>
2023-01-31 14:15:42 -0700

    Fix type error when checking for invalid (NaN) temp

--- components/Current.tsx
+++ components/Current.tsx
@@ -53,7 +53,7 @@ const Current = ({ temp, info }: CurrentProps) => {
 
     return (
         <div id="current-conditions" className="font-interstate absolute top-current-t left-0 h-current w-current text-left z-current">
-            {(temp !== NaN) ? <>
+            {!Number.isNaN(temp) ? <>
                 <div id="current-now" className="text-now absolute top-now-t left-now-l tracking-now text-left transform scale-x-105 scale-y-100 origin-left font-interstate font-bold text-shadow-none text-right text-dark z-now">now</div>
                 <div
                     id="current-nowwide"
